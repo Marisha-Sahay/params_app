9b7f690b84370c2df891ee34570d10a721a95eb5
Marisha Sahay <marishasahay@gmail.com>
2016-07-24 14:08:51 -0700

    add forms

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -1,5 +1,67 @@
 class ParamsController < ApplicationController
 def params_query
+  @output = ""
+  @message = params[:message]
+  if @message[0] == "a" || @message[0] == "A"
+    @output = "Hey, your name starts with the first letter of the alphabet!"
+  end
+end
+
+def guess
+  @output = ""
+  answer = 67
+  number = params[:no].to_i
+  if number == answer
+    @output = "Correct guess!!!!"
+  elsif number > answer
+    @output = "Guess lower"
+  elsif number < answer
+    @output = "Guess higher"
+  end
+  end
+
+  def count
+    @view_number = params[:number].to_i + 1
+  end
+
+  def url_segment_parameters
+    @message = params[:message]
+  end
+
+  def guess_segment_param
+  @output = ""
+  answer = 67
+  number = params[:number].to_i
+  if number == answer
+    @output = "Correct guess!!!!"
+  elsif number > answer
+    @output = "Guess lower"
+  elsif number < answer
+    @output = "Guess higher"
+  end
+  end
+
+  def form_display
+  end
+
+def form_result
   @message = params[:message]
 end
+
+def number_form
+end
+
+def number_result
+  number = params[:number].to_i
+  answer  = 78
+  if number == answer
+    @output = "Correct guess!!!!"
+  elsif number > answer
+    @output = "Guess lower"
+  elsif number < answer
+    @output = "Guess higher"
+  end
+end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   get "params_query", to: "params#params_query" 
-end
+  get "guess", to: "params#guess"
+  get "count", to: "params#count"
+  #Url Segment parameters
+  get "/url_segment_parameters/:message", to: "params#url_segment_parameters"
+  get "/guess_segment_param/:number", to: "params#guess_segment_param"
+  #forms parameters
+  get "/form_display", to: "params#form_display"
+  post "/form_result", to: "params#form_result"
+  #forms parameters
+  get '/number_form', to: 'params#number_form'
+  post '/number_result', to: 'params#number_result'
+  end
